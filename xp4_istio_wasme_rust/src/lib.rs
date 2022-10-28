5404b0c8a60aaa0f457904ba4038f64b831da4a3
dxps <marius.ileana@gmail.com>
2022-10-28 20:51:24 +0300

    xp4: beginnings

---
+++ xp4_istio_wasme_rust/src/lib.rs
@@ -0,0 +1,72 @@
+use log::debug;
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+
+proxy_wasm::main! {{
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(HttpHeadersRoot) });
+}}
+
+struct HttpHeadersRoot;
+
+impl Context for HttpHeadersRoot {}
+
+impl RootContext for HttpHeadersRoot {
+    fn get_type(&self) -> Option<ContextType> {
+        Some(ContextType::HttpContext)
+    }
+
+    fn create_http_context(&self, context_id: u32) -> Option<Box<dyn HttpContext>> {
+        Some(Box::new(HttpHeaders { context_id }))
+    }
+}
+
+struct HttpHeaders {
+    context_id: u32,
+}
+
+impl Context for HttpHeaders {}
+
+impl HttpContext for HttpHeaders {
+    fn on_http_request_headers(&mut self, _: usize, _: bool) -> Action {
+        for (name, value) in &self.get_http_request_headers() {
+            debug!("#{} -> {}: {}", self.context_id, name, value);
+        }
+
+        match self.get_http_request_header(":path") {
+            Some(path) if path == "/hello" => {
+                self.send_http_response(
+                    200,
+                    vec![("Hello", "World"), ("Powered-By", "proxy-wasm")],
+                    Some(b"Hello, World!\n"),
+                );
+                Action::Pause
+            }
+            _ => Action::Continue,
+        }
+    }
+
+    fn on_http_response_headers(&mut self, _: usize, _: bool) -> Action {
+        let mut authority = "";
+        let headers = self.get_http_request_headers();
+        for (name, value) in &headers {
+            debug!("#{} <- {}: {}", self.context_id, name, value);
+            if name == "authority" {
+                authority = value.as_str();
+            }
+        }
+        self.set_http_response_header("x-hello", Some(&format!("Hello {}", authority)));
+        self.set_http_response_header(
+            "",
+            Some(&format!(
+                "xp3_istio_wasme_rust_hello_header #{}",
+                self.context_id
+            )),
+        );
+        Action::Continue
+    }
+
+    fn on_log(&mut self) {
+        debug!("#{} completed.", self.context_id);
+    }
+}
