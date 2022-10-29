3a426867c0a56236da706d8b190905faf10986fb
dxps <marius.ileana@gmail.com>
2022-10-29 19:21:26 +0300

    xp4: wip

--- xp4_istio_wasme_rust/readme.md
+++ xp4_istio_wasme_rust/readme.md
@@ -1,2 +1,28 @@
 ## Experiment 4
 
+### `proxy-wasm`'s Concepts and Features
+
+Proxy-Wasm supports two types of extensions:
+1. Filters
+   - An instance exists and is run within each (Envoy) worker thread.
+2. Singleton services
+   - One instance of it exists and gets executed in the main thread, outside the request lifecycle.
+
+#### Contexts
+
+For each request, a context object gets created. There are three types of contexts:
+1. `RootContext`
+   - This context can read the (WASM)VM configuration, read plugin configuration, set tick periods, create context, and any other work outside the request lifecycle.
+   - One instance gets created for each thread in a plugin.
+2. `StreamContext`
+   - This context is used when writing network filters.
+   - One instance gets created for each request.
+3. `HTTPContext`
+   - This context is used when writing HTTP filters.
+   - One instance gets created for each request.1
+
+#### Shared Data
+
+Shared data is an in-memory key-value store, specified by the Proxy-Wasm ABI and provided by the proxy. Each VM contains a shared datastore.
+
+The data exchanged between the host and VM is done in binary format, therefore, it requires serialization and deserialization.
--- xp4_istio_wasme_rust/src/lib.rs
+++ xp4_istio_wasme_rust/src/lib.rs
@@ -1,3 +1,5 @@
+use std::string;
+
 use log::debug;
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
@@ -21,6 +23,8 @@ impl RootContext for HttpHeadersRoot {
     }
 }
 
+const SHARED_KEY_HEADER: &str = "sk";
+
 struct HttpHeaders {
     context_id: u32,
 }
@@ -28,9 +32,32 @@ struct HttpHeaders {
 impl Context for HttpHeaders {}
 
 impl HttpContext for HttpHeaders {
-    fn on_http_request_headers(&mut self, _: usize, _: bool) -> Action {
+    fn on_http_request_headers(&mut self, _hdrs_cnt: usize, _eos: bool) -> Action {
         for (name, value) in &self.get_http_request_headers() {
             debug!("#{} -> {}: {}", self.context_id, name, value);
+        }
+
+        if let Some(sk) = self.get_http_request_header(SHARED_KEY_HEADER) {
+            let (data_opt, cas) = &self.get_shared_data(SHARED_KEY_HEADER);
+            // cas (abreviation for compare-and-switch) is used when updating the value.
+            debug!(
+                "#{} -> received {}={:?}",
+                self.context_id, SHARED_KEY_HEADER, data_opt
+            );
+            let resp_sk = match data_opt {
+                Some(prev_data) => format!("{:?}|{:?}", prev_data, sk),
+                None => sk,
+            };
+            match &self.set_shared_data(SHARED_KEY_HEADER, Some(resp_sk.as_bytes()), *cas) {
+                Ok(_) => debug!(
+                    "#{} -> set {}={}",
+                    self.context_id, SHARED_KEY_HEADER, resp_sk
+                ),
+                Err(e) => debug!(
+                    "#{} -> failed to set {} due to '{:?}'",
+                    self.context_id, SHARED_KEY_HEADER, e
+                ),
+            }
         }
 
         match self.get_http_request_header(":path") {
